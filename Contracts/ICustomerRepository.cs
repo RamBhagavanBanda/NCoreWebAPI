be096e36f4781abb46c867febc91c32020d4e416
Ram Banda <brbd@hotmail.com>
2018-04-26 01:45:58 -0700

    initial check in

---
+++ Contracts/ICustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using WebApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Contracts
+{
+    public interface ICustomerRepository
+    {
+        void Add(Customer item);
+
+        IEnumerable<Customer> GetAll();
+
+        Customer Find(int key);
+
+        Customer Remove(int key);
+
+        void Update(Customer item);
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebApplication1.Contracts;
+using WebApplication1.Models;
+using WebApplication1.Repositories;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var con = "server=.;database=H_Plus_Sports;trusted_connection=true;";
+            services.AddDbContext<H_Plus_SportsContext>(options => options.UseSqlServer(con));
+
+          
+           services.AddSingleton<ICustomerRepository, CustomerRepository>();
+         //   services.AddSingleton<ISalespersonRepository, SalespersonRepository>();
+          //  services.AddSingleton<IProductRepository, ProductRepository>();
+         //   services.AddSingleton<IOrderRepository, OrderRepository>();
+          //  services.AddSingleton<IOrderItemRepository, OrderItemRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
